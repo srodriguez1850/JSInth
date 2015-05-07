1aa264a10fe5c5de9688962e2d6ae8c58fd91e2f
swms <SpencerWilliams2016@u.northwestern.edu>
2015-05-06 20:33:17 -0500

    Change A Wave math

--- Wave Math/sine_generation_script.py
+++ Wave Math/sine_generation_script.py
@@ -4,12 +4,12 @@ import xlrd
 book = xlrd.open_workbook("Tone_calculator.xlsx")
 print "The number of worksheets is", book.nsheets
 print "Worksheet name(s):", book.sheet_names()
-sh = book.sheet_by_index(1)
+sh = book.sheet_by_index(2)
 print sh.name, sh.nrows, sh.ncols
 
 f = open('A4.txt', 'w')
 
-for x in range(1, 111):
+for x in range(1, 77):
 	#print 'X"', sh.cell_value(rowx=x, colx=6), '" when "', sh.cell_value(rowx=x, colx=7), '",'
 	f.write('X"')
 	f.write(sh.cell_value(rowx=x, colx=6))
@@ -17,5 +17,5 @@ for x in range(1, 111):
 	f.write(sh.cell_value(rowx=x, colx=7))
 	f.write('",\n')
 
-
+f.write('X"0000" when others;')
 f.close()
