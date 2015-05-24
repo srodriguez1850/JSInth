7d5131d3dada38c9ca570618cebb9b317144169b
Ian Feeney <feeney2k@users.noreply.github.com>
2015-05-24 13:55:59 -0500

    reverb modified

--- reverb_tb.vhd
+++ reverb_tb.vhd
@@ -3,15 +3,16 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.constants.all;
 
-entity reverb is
+entity reverb_tb is
 	port(
-		source_signal : in std_logic_vector(DATA_SIZE - 1 downto 0);
-		clear_bit, clk, switch: in std_logic;
-		output: out std_logic_vector(DATA_SIZE - 1 downto 0)
+		source_signal, switch : in std_logic_vector(DATA_SIZE - 1 downto 0);
+		clear_bit, clk: in std_logic;
+		flanger_output: out std_logic_vector(DATA_SIZE - 1 downto 0)
 		);
-end entity reverb;
 
-architecture behavioral of reverb is
+end entity reverb_tb;
+
+architecture behavioral of reverb_tb is
 
 	component reverb_FIFO is
 		generic(
@@ -26,6 +27,7 @@ architecture behavioral of reverb is
 			data_out : out std_logic_vector (DATA_SIZE - 1 downto 0);
 			empty : out std_logic;
 			full : out std_logic);
+			
 	end component reverb_FIFO;
 	
 	component reverb_adder is
@@ -42,20 +44,18 @@ architecture behavioral of reverb is
 	
 	
 	signal fifo_output, and_output: std_logic_vector(DATA_SIZE - 1 downto 0);
+	
 	signal fifo_reset, fifo_write_en, fifo_read_en, fifo_empty, fifo_full : std_logic;
 	signal fifo_in_data, fifo_out_data : std_logic_vector (DATA_SIZE - 1 downto 0);
+	signal and_result : std_logic_vector(DATA_SIZE - 1 downto 0);
 	
 	begin
 	
 	FIFO_0: reverb_FIFO port map(clk, fifo_reset, fifo_write_en, fifo_read_en, fifo_in_data, fifo_out_data, fifo_empty, fifo_full);
 	
-	AND_0: AND16 port map(fifo_out_data, switch, and_output);
+	AND_0: AND16 port map(fifo_out_data, switch, and_result);
 	
-	ADDER_0: reverb_adder port map(source_signal, and_output, ouput);
-	  
-	if (clear_bit = 1) then
-	  fifo_reset <= 1;
-	  
+	ADDER_0: reverb_adder port map(source_signal, and_output, flanger_output);
 	
 	
 end behavioral;
